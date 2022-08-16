8e67c52219a5b3cfa11a796c6def29eb073b12a5
Carol Maser <92386477+CarolMaser@users.noreply.github.com>
2022-08-16 10:14:05 -0300

    Desconto a partir de um determinado valor.
    Usuário informa o preço e caso o valor for igual e/ou maior que R$500,00, o programa retorna o valor de desconto obtido.

---
+++ pbm02/ex14/ex14.php
@@ -0,0 +1,21 @@
+<?php
+
+    $valor = $_GET["valor"];
+    $desc = ($valor/100)*10;
+    $nvalor = $valor-$desc;
+
+    echo "Você comprou R$" . $valor . " na nossa loja. OBRIGADO!";
+    if ($valor < 500) {
+        echo "<br><br>Volte sempre!";
+    } else {
+        echo "<br><br>========== ATENÇÃO ==========";
+        echo "<br><br>Por fazer mais de R$500,00 em compras,";
+        echo "<br>você vai receber R$" . round($desc, 2) . " de desconto.";
+        echo "<br><br>O valor a ser pago será de R$" . round($nvalor, 2) . "!";
+        echo "<br><br>Volte sempre!";
+    }
+
+    
+    
+
+?>
